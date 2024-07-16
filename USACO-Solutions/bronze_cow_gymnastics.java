5dd43bd31b9a14d7b4769ac9a5fb570bc7119282
David (Yiming) Xiong <88404697+Helsinki1@users.noreply.github.com>
2024-07-16 15:20:44 -0400

    Update bronze_cow_gymnastics.java

--- USACO-Solutions/bronze_cow_gymnastics.java
+++ USACO-Solutions/bronze_cow_gymnastics.java
@@ -10,30 +10,30 @@ public class bronze_cow_gymnastics{
 		int numLines = Integer.parseInt(st.nextToken());
 		int numCows = Integer.parseInt(st.nextToken());  // cows will be labeled from 1 to N (subtract 1 to index them)
 
-        int [][] cowRankings = new int [numLines][numCows];
-        for(int a=0; a<numLines; a++){
-            String [] line = (r.readLine()).split(" ");
-            for(int b=0; b<numCows; b++){
-                int cowIdentity = Integer.parseInt(line[b]);
-                cowRankings[a][cowIdentity-1] = b;
-            }
-        }
+        	int [][] cowRankings = new int [numLines][numCows];
+	        for(int a=0; a<numLines; a++){
+	            String [] line = (r.readLine()).split(" ");
+	            for(int b=0; b<numCows; b++){
+	                int cowIdentity = Integer.parseInt(line[b]);
+	                cowRankings[a][cowIdentity-1] = b;
+	            }
+	        }
+	
+	        int count = 0;
+	        for(int a=0; a<numCows; a++){
+	            for(int b=0; b<numCows; b++){
+	                boolean consistent = true;
+	                for(int i=0; i<numLines; i++){
+	                    if(cowRankings[i][a] >= cowRankings[i][b]){
+	                        consistent = false;
+	                    }
+	                }
+	                if(consistent) count++;
+	            }
+	        }
+	        
+	        pw.println(count);
+	        pw.close();
 
-        int count = 0;
-        for(int a=0; a<numCows; a++){
-            for(int b=0; b<numCows; b++){
-                boolean consistent = true;
-                for(int i=0; i<numLines; i++){
-                    if(cowRankings[i][a] >= cowRankings[i][b]){
-                        consistent = false;
-                    }
-                }
-                if(consistent) count++;
-            }
-        }
-        
-        pw.println(count);
-        pw.close();
-
-    }
+    	}
 }
