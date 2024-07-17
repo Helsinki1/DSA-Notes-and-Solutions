0edfd537cabc68c0a2345aa4de40370dcb9d1d57
David (Yiming) Xiong <88404697+Helsinki1@users.noreply.github.com>
2024-07-17 18:54:29 -0400

    Update bronze_why_cow_cross_road.java

--- USACO-Solutions/bronze_why_cow_cross_road.java
+++ USACO-Solutions/bronze_why_cow_cross_road.java
@@ -7,28 +7,28 @@ public class bronze_why_cow_cross_road{
 		PrintWriter pw = new PrintWriter("circlecross.out");
 
 		String crossings = r.readLine();
-        // if two letters appear like A/B/A/B then they cross, if A/A/B/B then no
-        
-        String [] cows = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-        int count = 0;
-        for(int a=0; a<26; a++){
-            for(int b=0; b<26; b++){
-                boolean cowsCross = true;
-                String prev = " ";
-                for(int i=0; i<52; i++){
-                    if(crossings.substring(i,i+1).equals(cows[a]) || crossings.substring(i,i+1).equals(cows[b])){
-                        if(prev.equals(crossings.substring(i,i+1))){
-                            cowsCross = false;
-                        }
-                        prev = crossings.substring(i,i+1);
-                    }
-                }
-                if(cowsCross) count++;
-            }
-        }
-
-        pw.println(count/2);
-        pw.close();
-    }
+	        // if two letters appear like A/B/A/B then they cross, if A/A/B/B then no
+	        
+	        String [] cows = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+	        int count = 0;
+	        for(int a=0; a<26; a++){
+	            for(int b=0; b<26; b++){
+	                boolean cowsCross = true;
+	                String prev = " ";
+	                for(int i=0; i<52; i++){
+	                    if(crossings.substring(i,i+1).equals(cows[a]) || crossings.substring(i,i+1).equals(cows[b])){
+	                        if(prev.equals(crossings.substring(i,i+1))){
+	                            cowsCross = false;
+	                        }
+	                        prev = crossings.substring(i,i+1);
+	                    }
+	                }
+	                if(cowsCross) count++;
+	            }
+	        }
+	
+	        pw.println(count/2);
+	        pw.close();
+	    }
 }
 	
